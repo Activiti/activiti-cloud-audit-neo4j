10257b6fdc76f9224ed90fcb7c164d703190b498
Mario Romano <dnamario@hotmail.com>
2018-10-05 19:49:35 +0100

    init spring boot ms

---
+++ src/main/java/org/activiti/audit/ActivitiCloudAuditNeo4jApplication.java
@@ -0,0 +1,12 @@
+package org.activiti.audit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiCloudAuditNeo4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiCloudAuditNeo4jApplication.class, args);
+	}
+}
